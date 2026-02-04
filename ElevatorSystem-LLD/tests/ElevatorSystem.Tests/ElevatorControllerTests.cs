a21b03937cccd59a8afe518577247405c407253c
jasim <tjasim786@gmail.com>
2026-02-04 11:06:23 +0530

    test: add idle and door safty tests

--- ElevatorSystem-LLD/tests/ElevatorSystem.Tests/ElevatorControllerTests.cs
+++ ElevatorSystem-LLD/tests/ElevatorSystem.Tests/ElevatorControllerTests.cs
@@ -12,10 +12,7 @@ namespace ElevatorSystem.Tests
         public void Elevator_Moves_Up_To_Target_Floor()
         {
             // Arrange
-            var elevators = new List<Elevator>
-    {
-        new Elevator(1)
-    };
+            var elevators = new List<Elevator> { new Elevator(1) };
             var strategy = new NearestElevatorStrategy();
             var controller = new ElevatorController(elevators, strategy);
 
@@ -38,6 +35,63 @@ namespace ElevatorSystem.Tests
             // Assert
             Assert.Equal(3, elevators[0].CurrentFloor);
             Assert.True(doorOpened);
+        }
+
+        [Fact]
+        public void Elevator_Does_Not_Move_When_No_Request()
+        {
+            //Arrange
+            var elevators = new List<Elevator> { new Elevator(3) };
+            var strategy = new NearestElevatorStrategy();
+            var controller = new ElevatorController(elevators, strategy);
+
+            //Act
+            for(int i = 0; i < 5; i++)
+            {
+                controller.Step();
+            }
+
+            //Assert
+            Assert.Equal(0,elevators[0].CurrentFloor);
+            Assert.Equal(Direction.Idle, elevators[0].Direction);
+            Assert.Equal(DoorState.Closed, elevators[0].Door.State);
+
+        }
+
+        [Fact]
+        public void Door_Closes_Before_Elevator_Moves_Again()
+        {
+            // Arrange
+            var elevator = new Elevator(1);
+            var elevators = new List<Elevator> { elevator };
+
+            var strategy = new NearestElevatorStrategy();
+            var controller = new ElevatorController(elevators, strategy);
+
+            controller.RequestElevator(1, Direction.Up);
+            controller.RequestElevator(3, Direction.Up);
+
+            bool doorOpened = false;
+            bool doorClosedAfterOpen = false;
+
+            // Act
+            for (int i = 0; i < 10; i++)
+            {
+                controller.Step();
+
+                if (elevator.Door.State == DoorState.Open)
+                    doorOpened = true;
+
+                if (doorOpened && elevator.Door.State == DoorState.Closed && elevator.CurrentFloor > 1)
+                {
+                    doorClosedAfterOpen = true;
+                    break;
+                }
+            }
+
+            // Assert
+            Assert.True(doorOpened);
+            Assert.True(doorClosedAfterOpen);
         }
 
     }
